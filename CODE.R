fc5ef2eeb6692933f83cb38a8492a56081099407
rishi-gurjar <rishigurjar@icloud.com>
2022-06-11 17:13:24 -0700

    new

--- CODE.R
+++ CODE.R
@@ -23,8 +23,8 @@ datLong1 <- read.csv("/Users/rishigurjar/datLong1.csv")
 
 ggplot(datLong1, aes(x=year, y=percentage, fill=Series.Name)) + 
   geom_area(alpha=0.6 , size=1, colour="black")+
-  scale_fill_viridis(discrete = T) +
-  labs(x= "Year", y = "Percentage (Volume measured in metric tons)", title = "Percentage Seafood Production Share in the US 1971-2018")
+  labs(x= "Year", y = "Percentage (Volume measured in metric tons)", title = "Percentage Seafood Production Share in the US 1971-2018")+
+  scale_fill_manual("Fishery Types", labels = c("Aquaculture" , "Capture Fishery"), values = c("#440154", "#5ec962"))
 
 #-------
 importsLong <- read.csv("/Users/rishigurjar/Desktop/SeafoodResearch/fosslandings.csv")
@@ -37,7 +37,7 @@ ggplot(importsLong, aes(Year, as.numeric(as.character(MetricTons)), group = 1)) 
   labs(x= "Year", y = "Metric Tons", title = "US Commercial Seafood Landings 1950-2020")
 #------
 consumpLong <- read.csv("/Users/rishigurjar/Desktop/aquaresearchFinal/percapconsumption.csv")
-
+view(consumpLong)
 ggplot(consumpLong) +
   geom_path(aes(x = Year, y = Val, color = Type)) +
   facet_grid(Type ~ ., scales = "free_y", 
@@ -49,6 +49,19 @@ ggplot(consumpLong) +
   guides(color = "none") +
   labs(title = "US Population Growth and Annual Per Capita Consumption of Seafood Products 1910-2019")
 
+coeff <- 15
+
+ggplot(consumpLong, aes(x = Year))+
+  geom_line(aes(y = Pop), size = 1, color = "blue")+
+  geom_line(aes(y = Pounds*coeff), size = 1, color = "red")+
+  scale_y_continuous(
+    name = "Population (in millions)",
+    sec.axis = sec_axis(trans=~./coeff, name = "Pounds (lbs)")) + 
+    labs(title = "US Population Growth and Annual Per Capita Consumption of Seafood Products (1910-2019)")+
+  theme(
+    axis.title.y = element_text(color = "blue", size=10),
+    axis.title.y.right = element_text(color = "red", size=10))
+
 #---------
 aquaCap <- read.csv("/Users/rishigurjar/Desktop/SeafoodResearch/oced_aquacultureprodmod1.csv")
 #View(aquaCap)
@@ -57,11 +70,11 @@ ggplot(aquaCap, aes(x=TIME, y=PerCapita, color=LOCATION)) +
   geom_line(size = 1.5)+
   labs( x = "Time (yrs)",
         y = "Volume (tonnes)",
-        title ="Per Capita Aaquaculture Production Volume (1995-2019)") + 
-  scale_color_hue(labels = c("China", "Indonesia", "Thailand", "United States", "Vietnam"))
-
+        title ="Per Capita Aquaculture Production Volume (1995-2019)") + 
+  scale_color_hue("Countries", labels = c("China", "Indonesia", "Thailand", "United States", "Vietnam"))
 #---------
 discards <- read.csv("/Users/rishigurjar/Desktop/aquaresearchFinal/discards.csv")
+view(discards)
 
 ggplot(discards, aes(x=Year, y=Discards, color=Code)) + 
   geom_line(size = 1)+
@@ -70,13 +83,15 @@ ggplot(discards, aes(x=Year, y=Discards, color=Code)) +
         title ="Fish discards by country 1950-2018",
         subtitle ="Discards are animals thrown back (alive or dead) into the sea after being caught during fishing activities.")
 
-
-ggplot(discards, aes(x=Year, y=PerCap, color=Code)) + 
+clean_discards <- filter(discards, Year >=1960)
+view(clean_discards)
+ggplot(clean_discards, aes(x=Year, y=PerCap, color=Code)) + 
   geom_line(size = 1)+
   labs( x = "Time (yrs)",
         y = "Volume (tonnes)",
-        title ="Fish discards per capita by country 1960-2018",
-        subtitle ="Discards are animals thrown back (alive or dead) into the sea after being caught during fishing activities.")
+        title ="Fish Discards Per Capita By Country (1960-2018)",
+        subtitle ="Discards are animals thrown back (alive or dead) into the sea after being caught during fishing activities.")+
+  scale_color_discrete(name = "Country", labels = c("China", "Great Britain", "Indonesia", "India", "United States", "Vietnam"))
 
 #---------
 captureprod <- read.csv("/Users/rishigurjar/Desktop/aquaresearchFinal/API_ER.FSH.CAPT.MT_DS2_en_csv_v2_3692271.csv")
@@ -106,8 +121,8 @@ topnet <- filter(toppop, name == 'Net')
 #view(topnet)
 
 ggplot(topnet, aes(x=Time, y=NetVal, color=country)) + 
-  geom_point()+
   geom_line(size = 1)+
   labs( x = "Time (yrs)",
-        y = "Net Volume (Aquaculture minus Capture Fishery) (mt)",
-        title ="Top 6 Population Countries Net (Aquaculture vs Capture Fishery) Per Capita")
+        y = "Net Volume (metric tons)",
+        title ="Top 6 Population Countries Net Fishery Production (Aquaculture vs Capture Fishery) Per Capita")+
+  scale_color_discrete("Country")
